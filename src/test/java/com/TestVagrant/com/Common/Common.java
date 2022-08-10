887941c4f8732375f18a9a7c58f7744ce8e2dd3f
KishoraRepo <kishora1204@gmail.com>
2022-08-10 22:31:21 +0530

    Coding Assignment 1 - Completed - Commit 2 - AUG 10/22

--- src/test/java/com/TestVagrant/com/Common/Common.java
+++ src/test/java/com/TestVagrant/com/Common/Common.java
@@ -1,36 +1,45 @@
 package com.TestVagrant.com.Common;
 
 import java.io.*;
-import java.util.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
+import org.junit.Assert;
 
+public class Common {
 
-public class Common extends BasePage {
-
-//    Read the data from the json and return it as ArrayList
-    public ArrayList readJson(String str) {
+    //    Read the data from the json and return it as ArrayList
+    public JSONArray readJson(String str) {
         JSONParser parser = new JSONParser(); // to read the file
-        ArrayList<String> playerString = null; // return ArrayList
+        JSONArray players = null; // to store the JSONArray of all players
         try {
             Object obj = parser.parse(new FileReader("C:/Automation Learning/TestVagrant/src/test/resources/uploads/" + str));
             JSONObject jsonObject = (JSONObject) obj; // converting it into Json Object
             String name = (String) jsonObject.get("name");
             String location = (String) jsonObject.get("location");
-            JSONArray players = (JSONArray) jsonObject.get("player"); // Seperating the JSONArray from the Main JSON Object
+            players = (JSONArray) jsonObject.get("player"); // Store the JSONArray separately
 
 //            Printing Team Name and Location
             System.out.println("Team Name: " + name);
             System.out.println("Location: " + location);
-
-            playerString = new ArrayList<>(); // create object for ArrayList
-
-            for (Object player : players) { // traverse through each Object from the JSONArray
-                playerString.add((String) player); // convert Object to String and add it to ArrayList of String type
-            }
         } catch (Exception e) {
             e.printStackTrace();
+            Assert.fail();
         }
-        return playerString; // Return the final ArrayList created
+        return players; // Return the final ArrayList created
+    }
+
+//    Count the total number of foreign players in Team and return the count
+    public int countForeignPlayers(JSONArray rcbTeam) {
+        int count = 0;
+        String country = "India";
+        for (Object player : rcbTeam) { // iterate each JSONObject from given JSONArray
+            String playerCountry = (String) ((JSONObject) player).get("country"); // Store the country value for each Player
+
+//            Check for the player who is foreigner
+            if (!playerCountry.equalsIgnoreCase(country)) {
+                count++; // If the player is foreigner increase the count
+            }
+        }
+        return count;
     }
 }
--- src/test/java/com/TestVagrant/com/StepDefs/CommonStepDefs.java
+++ src/test/java/com/TestVagrant/com/StepDefs/CommonStepDefs.java
@@ -3,13 +3,13 @@ package com.TestVagrant.com.StepDefs;
 import com.TestVagrant.com.Common.Common;
 import io.cucumber.java.af.En;
 import io.cucumber.java.en.Given;
-
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.json.simple.JSONArray;
 import java.lang.annotation.Annotation;
-import java.util.ArrayList;
+import static org.testng.Assert.assertTrue;
 
 public class CommonStepDefs extends Common implements En {
-
-    public ArrayList<String> playerString = new ArrayList<>();
 
     @Override
     public String value() {
@@ -21,9 +21,25 @@ public class CommonStepDefs extends Common implements En {
         return null;
     }
 
-        @Given("I read the {string} file")
+    public JSONArray playerString = new JSONArray();
+    public int countOfForeignPlayers;
+
+    @Given("I read the {string} file")
     public void iReadTheRCBTeamJsonFile(String str) {
         Common commonObject = new Common(); // creating object for java class Common
-            playerString = commonObject.readJson(str); // calling readJson method to read the data from json file and store
+        playerString = commonObject.readJson(str); // calling readJson method to read the data from json file and store
+    }
+
+    @When("I count the number of foreign players")
+    public void iCountTheNumberOfForeignPlayers() {
+        Common commonObject = new Common();
+        countOfForeignPlayers = commonObject.countForeignPlayers(playerString); // calling method to count the number of foreign players in the team
+    }
+
+    @Then("Total number of foreign players should match with number {int}")
+    public void totalNumberOfForeignPlayersShouldMatchWithNumber(int foreignPlayerCount) {
+//        Verify whether the count of foreign players in RCB Team matches the expected count
+        assertTrue(foreignPlayerCount==countOfForeignPlayers, "EXPECTED foreign players: " +
+                foreignPlayerCount + " but FOUND: " + countOfForeignPlayers);
     }
 }
